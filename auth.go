ed58d56f7b5a452c460e4a41fa8a22cf9f8b005a
HoLLy-HaCKeR <dario.db@hotmail.com>
2020-01-23 15:24:29 +0100

    Add gRPC, use them for auth

--- auth.go
+++ auth.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"io"
+	"context"
+	"errors"
+	"fmt"
+	"google.golang.org/grpc"
 	"net/http"
+
+	pb "github.com/acubed-tm/edge/protofiles"
 )
+
+// TODO: don't hardcode, use service discovery
+const authIp = "51.136.77.127:50551"
 
 func doLogin(w http.ResponseWriter, r *http.Request) {
 	var req struct {
@@ -17,8 +25,20 @@ func doLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO
-	// writeSuccessJson()
+	success, err := runGrpc(authIp, func(ctx context.Context, conn *grpc.ClientConn) (interface{}, error) {
+		// Contact the server and print out its response.
+		c := pb.NewLoginServiceClient(conn)
+		resp, err := c.Login(ctx, &pb.LoginRequest{Email: req.Email, Password: req.Password})
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("could not log in: %v", err))
+		}
+		return resp.Success, nil
+	})
 
-	_, _ = io.WriteString(w, "nice")
+	if err != nil {
+		writeErrorJson(w, err)
+		return
+	}
+
+	writeSuccessJson(w, success)
 }
--- helpers.go
+++ helpers.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"google.golang.org/grpc"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 )
 
 func getJsonFromPostRequest(r *http.Request, v interface{}) error {
@@ -24,6 +28,7 @@ func getJsonFromPostRequest(r *http.Request, v interface{}) error {
 }
 
 func writeSuccessJson(w http.ResponseWriter, v interface{}) {
+	log.Printf("Returning success: %v", v)
 	var resp struct {
 		Value interface{} `json:"value"`
 	}
@@ -33,10 +38,28 @@ func writeSuccessJson(w http.ResponseWriter, v interface{}) {
 }
 
 func writeErrorJson(w http.ResponseWriter, e error) {
+	log.Printf("Returning error: %v", e.Error())
 	var resp struct {
 		Message string `json:"error"`
 	}
 	resp.Message = e.Error()
 	jsonBytes, _ := json.Marshal(&resp)
 	_, _ = io.WriteString(w, string(jsonBytes))
+}
+
+func runGrpc(ip string, f func(context.Context, *grpc.ClientConn) (interface{}, error)) (interface{}, error) {
+	log.Printf("Starting gRPC connection to %s", ip)
+	conn, err := grpc.Dial(ip, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("did not connect: %v", err))
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+
+	ret, err := f(ctx, conn)
+
+	cancel()
+	_ = conn.Close()
+
+	return ret, err
 }
