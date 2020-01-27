83ed7de9780eca95c5d50bcd102360dd19a9c8c4
HoLLy-HaCKeR <dario.db@hotmail.com>
2020-01-27 17:38:20 +0100

    Add register endpoint

--- auth.go
+++ auth.go
@@ -12,6 +12,37 @@ import (
 
 const authIp = "authenticationms.acubed:50551"
 
+func doRegister(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+		Token    string `json:"token"`
+	}
+
+	err := getJsonFromPostRequest(r, &req)
+	if err != nil {
+		writeErrorJson(w, err)
+		return
+	}
+
+	success, err := runGrpc(authIp, func(ctx context.Context, conn *grpc.ClientConn) (interface{}, error) {
+		// Contact the server and print out its response.
+		c := pb.NewAuthServiceClient(conn)
+		resp, err := c.Register(ctx, &pb.RegisterRequest{Email: req.Email, Password: req.Password, VerificationToken: req.Token})
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("could not log in: %v", err))
+		}
+		return resp.Success, nil
+	})
+
+	if err != nil {
+		writeErrorJson(w, err)
+		return
+	}
+
+	writeSuccessJson(w, success)
+}
+
 func doLogin(w http.ResponseWriter, r *http.Request) {
 	var req struct {
 		Email    string `json:"email"`
